9c2384782a1656ccb05a1db51e9cf388fea93e62
186coolboy <186coolboy@gmail.com>
2020-03-05 15:18:50 +0800

    调整支付线程

--- flt_pay_ali/example/lib/main.dart
+++ flt_pay_ali/example/lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flt_pay_ali/flt_pay_ali.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,32 +13,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  String _payInfo = "";
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
+  String _payResult;
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
+  Future<void> _pay() async {
     try {
-      platformVersion = await FltPayAli.aliPay("");
+      await FltPayAli.aliPay("", (String result) {
+        print('aliPay result : $result');
+        setState(() {
+          _payResult = result;
+        });
+      });
     } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      setState(() {
+        _payResult = 'Failed to get platform version.';
+      });
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -45,10 +37,26 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('aliPay plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Pay info:\n$_payInfo\n'),
+            ),
+            RaisedButton(
+              onPressed: _pay,
+              child: Text('pay'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('pay result : $_payResult'),
+            )
+          ],
         ),
       ),
     );
